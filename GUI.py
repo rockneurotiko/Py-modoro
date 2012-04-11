91a1115f0536191089f2e952cb36a16ec07be093
unknown <rock@.(none)>
2012-04-11 18:55:33 +0200

    GUI added, refactoring code.
    The code have already recode to the GUI interface.
    
    The GUI it's writen in Tkinter.
    
    Uses pygame to play the final sound.

---
+++ GUI.py
@@ -0,0 +1,280 @@
+#!/usr/bin/python
+
+from Tkinter import *
+import tkMessageBox
+from KThread import *
+from time import sleep
+import os
+
+
+
+__author__ = "Rock Neurotiko"
+__copyright__= "Copyright 2012, Rock Neurotiko"
+__credits__ = ["Rock Neurotiko", ]
+__license__ = "GNU/GPL v3"
+__version__ = "0.2"
+__maintainer__ = "Rock Neurotiko"
+__email__ = "miguelglafuente@gmail.com"
+
+class App:
+
+        def __init__(self, master, pom):
+                self.errors = Errors()
+
+                self.pom = pom
+                self.time_in_var = IntVar()
+                self.time_in_var.set(25)
+                self.time = StringVar()
+                self.time.set("There is no Pomodoro started.")
+
+                self.frame = Frame(master)
+                self.frame.pack()
+
+                label_time_input = Label(self.frame, text="Input the time.")
+                label_time_input.grid(row=2, column=0)
+
+                self.label_time = Label(self.frame, textvariable = self.time, cursor = "pirate")
+                self.label_time.grid(row=1, column=1)
+
+
+                scale_time = Scale(self.frame, variable = self.time_in_var, from_=1, to=60, resolution = 1.0)
+                scale_time.grid(column=3)
+
+
+                entry_time = Spinbox(self.frame, textvariable= self.time_in_var, width=15, from_=1, to=60)
+                entry_time.grid(row=2, column=1)
+
+                button_start = Button(self.frame, text="START", fg="green", command=self.start)
+                button_start.grid(row=3, column=0)
+
+                self.button_resume = Button(self.frame, text="RESUME", fg="blue", command=self.resume, state=DISABLED)
+                self.button_resume.grid(row=3, column=1)
+
+                self.button_stop = Button(self.frame, text= "STOP", fg="red", command=self.stop, state=DISABLED)
+                self.button_stop.grid(row=3, column=2)
+
+
+
+
+        def start(self):
+                try:
+                        self.pom.crear_pom(self.time_in_var.get() * 60)
+                        self.pom.iniciar_pom()
+                        global e
+                        e = KThread(target = self.print_time)
+                        e.start()
+
+                        self.button_stop.config(state=ACTIVE)
+                        self.button_resume.config(state=DISABLED)
+                except ValueError:
+                        self.errors.ValueError()
+
+        def resume(self):
+                try:
+                        self.pom.resume_pom()
+                        global e
+                        e = KThread(target = self.print_time)
+                        e.start()
+
+                        self.button_resume.config(state=DISABLED)
+                except ValueError:
+                        self.errors.ValueError()
+
+
+                
+        def stop(self):
+                self.pom.interrupt_pom()
+                e.kill()
+                self.button_resume.config(state=ACTIVE)
+
+        def end(self):
+                self.time.set("Congrats, you had already finish the pomodoro, take a break.")
+                self.button_stop.config(state=DISABLED)
+
+
+        def print_time(self):
+                while(self.pom.segundos_trans < self.pom.tiempo and self.pom.estado == "START"):
+                        self.time.set(self.pom.get_tiempo())
+                        sleep(0.25)
+                self.end()
+
+
+
+
+
+class Config:
+
+        def __init__(self, master):
+                self.master = master
+
+
+
+                #Volume Config
+                self.volume = DoubleVar()
+                self.volume.set(1.0)
+
+                volume_label = Label(master, text="Volume: ")
+                volume_label.grid(row=1, column=0)
+
+                volume_scale = Scale(master, variable = self.volume, from_=0.0, to=1.0, resolution = 0.1, orient=HORIZONTAL)
+                volume_scale.grid(row=1, column=1)
+
+
+                #Buttons to accept/deny the changes.
+                accept_button = Button(master, text="Accept", command = self.accept)
+                accept_button.grid(row=2, column=0)
+
+                cancel_button = Button(master, text="Cancel", command = self.cancel)
+                cancel_button.grid(row=2, column=1)
+
+        def accept(self):
+                pom.volume = self.volume.get()
+                self.master.destroy()
+
+        def cancel(self):
+                self.master.destroy()
+
+
+
+
+
+class TimePresets:
+
+        def __init__(self, master):
+                self.master = master
+                self.var = IntVar()
+
+                normal_pom_time = Radiobutton(master, text="Normal Pomodoro (25')", variable=self.var, value=25)
+                normal_pom_time.grid(row=0, column=1)
+
+                short_break = Radiobutton(master, text="Short Break (5')", variable=self.var, value=5)
+                short_break.grid(row=1, column=1)
+
+                long_break = Radiobutton(master, text="Long Break (15')", variable=self.var, value=15)
+                long_break.grid(row=2, column=1)
+
+                #Buttons to accept/deny the changes.
+                accept_button = Button(master, text="Accept", command = self.accept)
+                accept_button.grid(row=3, column=0)
+
+                cancel_button = Button(master, text="Cancel", command = self.cancel)
+                cancel_button.grid(row=3, column=2)
+
+        def accept(self):
+                app.time_in_var.set(self.var.get())
+                self.master.destroy()
+
+        def cancel(self):
+                self.master.destroy()             
+
+
+
+
+
+
+
+class Help:
+        def __init__(self, master):
+                help_text_var = StringVar()
+                help_text = Text(master, wrap=WORD, exportselection=0)
+                
+#                 help_text_var.set(""" Fellow pirate.
+# Do you wanna know what a Pomodoro is?
+# I'm gonna resume it, if you want a large explain search on the Internet""")
+                var = """ Fellow pirate.
+Do you wanna know what a Pomodoro is?
+I'm gonna resume it, if you want a large explain search on the Internet"""
+
+                help_text.insert(INSERT, var)
+                help_text.config(state=DISABLED)
+
+                help_text.pack()
+
+
+
+
+
+
+
+
+
+class Errors:
+        def __init__(self):
+                pass
+
+        def ValueError(self):
+                tkMessageBox.showinfo("Check-out your input. " + " " * 20 + "Py-Modoro", "You must introduce an entire number (given in minutes)")
+
+        def ImportError(self):
+                tkMessageBox.showerror("Error", "You must install the module pygame, check this link: http://www.pygame.org/download.shtml")
+                root.destroy()
+
+
+
+
+
+#Function to ask if you really wanna close.
+def callback():
+        if tkMessageBox.askokcancel("Quit" + " " * 20 + "Py-Modoro", "Are you sure you wanna close Py-Modoro?"):
+                root.destroy()
+
+
+
+#This function creates the configuration window (File>Configuration)
+def configuration():
+        configwindow = Toplevel(root)
+        Config(configwindow)
+
+
+def timePresets():
+        presetsWindow = Toplevel(root)
+        TimePresets(presetsWindow)
+
+
+#This function creates another window (Help>What's pomodoro?)
+def whatsPomodoro():
+        helpwindow = Toplevel(root) #Define the new window
+        Help(helpwindow) #Call to the class Help and pass the new window created.
+
+
+
+#Defines the menu
+def menu(root):
+        menubar = Menu(root)
+
+        configmenu = Menu(menubar, tearoff=0)
+        configmenu.add_command(label="Configuration", command=configuration)
+        configmenu.add_command(label="Time Presets", command=timePresets)
+        menubar.add_cascade(label="File", menu=configmenu)
+
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="What's Pomodoro?", command=whatsPomodoro)
+        helpmenu.add_command(label="About...", command=whatsPomodoro)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+        return menubar
+
+
+root = Tk()
+
+main_errors = Errors()
+
+try:
+        from pomodoro import pomodoro #this import is right here to catch the possible exception.
+except ImportError:
+        main_errors.ImportError()
+        os._exit(0)
+
+pom = pomodoro()
+
+root.title("Py-Modoro v.0.2  - By Rock Neurotiko")
+
+
+app = App(root, pom)
+
+menubar = menu(root)
+
+
+root.protocol("WM_DELETE_WINDOW", callback)
+root.config(menu=menubar)
+
+root.mainloop()
--- pomodoro.py
+++ pomodoro.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python
+
 from time import sleep
 import threading
 import play_sound
 from KThread import *
+import os, inspect, winsound, sys
+
+import pygame
 
 
 
@@ -21,7 +26,7 @@ from KThread import *
 #  			sleep(1)							#
 #  			self.contador += 1					#
 #												#
-#  	def segundos(self):							#
+#  	def seconds(self):							#
 #  		return self.time - self.contador		#
 #												#
 #################################################
@@ -40,79 +45,50 @@ from KThread import *
 
 __author__ = "Rock Neurotiko"
 __copyright__= "Copyright 2012, Rock Neurotiko"
-__credits__ = ["Rock Neurotiko", "Who mades the module KThread", 
-				"Who mades the module play_sound"]
+__credits__ = ["Rock Neurotiko",]
 __license__ = "GNU/GPL v3"
-__version__ = "0.1.2"
+__version__ = "0.2"
 __maintainer__ = "Rock Neurotiko"
 __email__ = "miguelglafuente@gmail.com"
 
 class pomodoro:
 
 	def __init__(self):
-		self.ESTADO = "STOP"
+
+		self.ESTADO = "STOP" #STATES: STOP, START, PAUSE, END
 		self.TIEMPO = 25*60
 		self.INTERR = 0
 		self.estado = self.ESTADO
 		self.tiempo = self.TIEMPO
 		self.interr = self.INTERR
 		self.on = False
-		self.count_t = 0
+		self.segundos_trans = 0
 
-	def contador(self):
-		global segundos_trans
-		segundos_trans = 0
-		while segundos_trans < self.tiempo:
-			segundos_trans += 1
-			sleep(1)
-
-	def segundos(self):
-		return self.tiempo - segundos_trans
-
-	def interruptions(self):
-		return self.interr
+		self.volume = 1.0
 
 
-	def terminar(self):
-		self.estado = "END"
-		self.on = False
-		play_sound.play("/home/rock/Programacion/pomodoro/alarma.mp3")
-		print "Fin del pomodoro, puedes descansar.\n"
-		self.promt_inicial()
+	def crear_pom(self, time = 0):
 
-
-	def arrancar(self, time):
-		global t
-		if self.on == True:
-			t.cancel()
-			self.on == False
-			self.count_t -= 1
-		t = threading.Timer(time, self.terminar)
-		t.start()
-		self.count_t += 1
-		self.on = True
-
-
-	def crear_pom(self, time = 25*60):
+		if (self.estado=="START" or self.estado=="PAUSE"):
+			self.interrupt_pom()
+			self.segundos_trans = 0 #reboot the seconds
 
 		self.estado="STOP"
+		if time==0:
+			time = 25*60
 		self.tiempo = time
-		
-
-	def tiempo_pom(self, mins = 0):
-		if mins == 0:
-			self.tiempo = 25*60
-		else:
-			self.tiempo = mins*60
 
 
 	def iniciar_pom(self):
-		
-		time = self.tiempo
+		#print("llama a iniciar")  #debug control
+
+		if(self.estado =="START"):
+			self.interrupt_pom()
+			self.segundos_trans = 0 #reboot the seconds
 
 		self.estado = "START"
 
-		self.arrancar(time)
+		self.arrancar(self.tiempo)
 
 		global e
 		e = KThread(target = self.contador)
@@ -120,25 +96,68 @@ class pomodoro:
 
 		self.interr = 0
 
-	
+	def resume_pom(self):
+		#print("llama a resumir") #debug control
+
+		self.estado = "START"
+
+		self.arrancar(self.tiempo)
+
+		global e
+		e = KThread(target = self.contador)
+		e.start()
+
+		self.interr = 0
 
 	def interrupt_pom(self):
-		if self.estado=="STOP":
+		#print "Llama a parar"  #debug control
+
+		if (self.estado=="STOP" or self.estado=="PAUSE"):
 			print "No puedes parar algo no empezado"
-		
 		else:
 			self.interr += 1
+			print "Pomodoro parado quedando: " + self.get_tiempo()
+			t.cancel() #kill the timer
+			self.on = False #turn off the state of t
+			e.kill() #kill the counter
+			self.estado = "PAUSE" #put the state into PAUSE
+
+	def arrancar(self, time):
+		global t
+		if self.on == True:
 			t.cancel()
-			e.kill()
-			print "Pomodoro parado quedando: " + self.a_minutos()
-			self.estado = "STOP"
+			self.on == False
+		t = threading.Timer(time, self.terminar)
+		t.start()
+		self.on = True
+
+	def terminar(self):
+		self.estado = "END"
+		e.kill()
+		self.on = False
+		self.segundos_trans = 0
+		print "Fin del pomodoro, puedes descansar.\n"
+		self.play_sound()
 
 
-	def a_minutos(self):
-		tiempo_local = self.segundos()
+
+
+	def contador(self):
+		sleep(1)
+		while (self.segundos_trans < self.tiempo and self.estado == "START"):
+			self.segundos_trans += 1
+			sleep(1)
+
+	def get_interruptions(self):
+		return self.interr
+
+	
+
+	def get_tiempo(self):
+		tiempo_local = self.seconds()
 
 		if tiempo_local <= 60:
-			return self.segundos() + " " + "segundos."
+			return str(self.seconds()) + " " + "segundos."  
 
 		elif tiempo_local <= 3600:
 			segundos_local = tiempo_local - (tiempo_local/60)*60
@@ -153,96 +172,23 @@ class pomodoro:
 			return (str(tiempo_local/3600) + " " + "hora/s, y " + str(minutos_local) + " " + 
 			"minuto/s, y " + str(segundos_local) + " " + "segundo/s.")
 
-	def saber_tiempo(self):
-		print "Quedan: " + self.a_minutos()
+
+	def seconds(self):
+		return self.tiempo - self.segundos_trans
+
+	def play_sound(self):
+
+		#so_system = sys.platform
+
+		pygame.mixer.init()
+		pygame.mixer.music.load('alarma.mp3')
+		pygame.mixer.music.set_volume(self.volume)
+		pygame.mixer.music.play()
 
 
+		# if(so_system[:3] == "win"):
+		# 	winsound.PlaySound('alarma.wav', winsound.SND_FILENAME)
+			
 
-	
- 	def cuenta_atras(self):
-
- 		while self.estado == "START":
- 			self.saber_tiempo()
- 			sleep(1)
- 			
- 		self.promt_inicial()
-
- 	
- 	def quit(self):
- 		if self.estado == "START":
- 			self.interrupt_pom()
- 		print "Espero que haya aprovechado el tiempo."
- 		raw_input()
-	
-
- 	#INTERFAZ
-
- 	def promt_iniciar(self):
- 		time = input("Introduzca tiempo en minutos.(si introduces 0 es por defecto [25])  >: ")
- 		self.tiempo_pom(time)
- 		self.iniciar_pom()
- 		self.promt_inicial()
-
-
- 	def promt_inicial(self):
-
-
- 		hacer = raw_input("Comando (h para ayuda): ")
-
- 		if (hacer == "--help" or hacer == "h"):
- 			print """ 
- 			Usted puede introducir:
- 			c -> muestra una cuenta atras del tiempo que queda(irreversible) [comando largo --cuenta]
- 			h -> muestra esta ayuda (comando largo --help)
- 			i -> Pasa a un promt donde introduces el tiempo para el pomodoro (comando largo --iniciar)
- 			--interrupciones -> muestra las interrupciones hechas al pomodoro.
- 			p -> para el pomodoro iniciado (comando largo --parar)
- 			q -> salir (comando largo --quit)
- 			t -> muestra el tiempo que queda de pomodoro (comando largo --tiempo)
- 			
-
-
- 			"""
- 			self.promt_inicial()
-
- 		elif (hacer == "--cuenta" or hacer == "c"):
- 			self.cuenta_atras()
-
- 		elif (hacer == "--iniciar" or hacer == "i"):
- 			self.promt_iniciar()
-
-		
- 		elif (hacer == "--parar" or hacer == "p"):
- 			self.interrupt_pom()
- 			self.promt_inicial()
-
-
- 		elif (hacer == "--quit" or hacer == "q"):
- 			if self.estado == "START":
- 				e.kill() 			
- 			self.quit()
-
- 		elif (hacer == "--tiempo" or hacer == "t"):
- 			if self.estado == "START":
- 				self.saber_tiempo()
- 				self.promt_inicial()
- 			else:
- 				print "No has iniciado ningun pomodoro. \n"
- 				self.promt_inicial()
- 		
-
-
- 		else:
- 			print """
- 			No ha introducido ningun comando valido.
- 			Introduzca "h" para ver los comandos habilitados.
- 			"""
- 			self.promt_inicial()
-
-
-
-
-
-pom = pomodoro()
-pom.promt_inicial()
-	
+		# elif(so_system[:3] == "lin"):
+		# 	play_sound.play(os.path.dirname(inspect.getfile(inspect.currentframe())) + "/alarma.wav")
